dd4a27375453976ba671ab6fdfbcde88f9645aba
Lorenzo <lorenzo.mussi@docomodigital.com>
2017-11-10 18:09:01 +0100

    hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HEROES } from './mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -14,16 +15,20 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-   
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
